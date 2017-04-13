7a037e820ac96f324d273efb77537a710042d4fa
vitaliiborodii <allterraintacticalenforcer@gmail.com>
2017-04-13 11:38:58 +0300

    impprovement

---
+++ horse.js
@@ -0,0 +1,235 @@
+const n = 200;
+
+var formKey = (x, y) => `${x}${y}`;
+
+var countSteps = (start, finish, N, launchAsync) => {
+
+  return new Promise((resolve, reject) => {
+
+    const maze = new Array(N);
+
+    for (let i = N; i--;) {
+      maze[i] = new Array(N);
+    }
+    const t = Date.now();
+    const twoMovesDist = 2 * Math.sqrt(5)// 2^2 + 1^2 - one horse move
+
+
+    const [xf, yf] = finish;
+
+    const createCoord = (x, y, prev) => ({
+      dist: Math.sqrt(Math.pow(xf - x, 2) + Math.pow(yf - y, 2)),
+      prev: prev,
+      value: [x, y]
+    });
+
+    let found = false;
+    let count = 0;
+
+    const visitNeighbours = (array, step) => {
+
+      const neighbours = array.reduce((map, coords) => {
+        const [px, py] = coords.value;
+
+        const key1 = formKey(px - 2, py - 1);
+        const key2 = formKey(px - 2, py + 1);
+        const key3 = formKey(px - 1, py - 2);
+        const key4 = formKey(px - 1, py + 2);
+        const key5 = formKey(px + 1, py - 2);
+        const key6 = formKey(px + 1, py + 2);
+        const key7 = formKey(px + 2, py - 1);
+        const key8 = formKey(px + 2, py + 1);
+
+
+        map.set(key1, createCoord(px - 2, py - 1, coords.value));
+        map.set(key2, createCoord(px - 2, py + 1, coords.value));
+        map.set(key3, createCoord(px - 1, py - 2, coords.value));
+        map.set(key4, createCoord(px - 1, py + 2, coords.value));
+        map.set(key5, createCoord(px + 1, py - 2, coords.value));
+        map.set(key6, createCoord(px + 1, py + 2, coords.value));
+        map.set(key7, createCoord(px + 2, py - 1, coords.value));
+        map.set(key8, createCoord(px + 2, py + 1, coords.value));
+
+        return map;
+      }, new Map());
+
+      const result = [];
+
+      const sortedInsert = (coord) => {
+        if (coord.dist >= twoMovesDist) {
+          result[0] = result[0] ? (result[0].dist > coord.dist ? coord : result[0]) : coord;
+        } else {
+          result.push(coord);
+        }
+      };
+
+      neighbours.forEach((coords) => {
+        const [px, py] = coords.value;
+        if (px < 0 || py < 0 || px >= N || py >= N) return false;
+
+        const cell = maze[px][py];
+
+        const value = cell && cell.value;
+
+        if (!value || ((step + 1) < value)) {
+          maze[px][py] = {
+            prev: coords.prev,
+            value: step + 1
+          };
+
+          if (px === xf && py === yf) {
+            found = true;
+          }
+
+          sortedInsert(coords);
+
+          return true;
+        } else {
+          return false
+        }
+      });
+      return result;
+    };
+
+
+    let nextSteps = [{value: start}];
+
+
+    const fn = () => {
+      nextSteps = visitNeighbours(nextSteps, count);
+      count++;
+    };
+
+    const syncFn = () => {
+
+      while (!found) {
+        fn()
+      }
+      resolve(maze);
+    };
+
+    const asyncFn = () => {
+      if (found) {
+        console.log(`${((Date.now() - t) / 1000).toFixed(3)} s`);
+        resolve(maze);
+      } else {
+        fn();
+
+        context.globalAlpha = 0.6;
+        context.fillStyle = "#000000";
+        nextSteps.forEach(coords => drawVisited(coords.value));
+        context.globalAlpha = 1;
+
+        setTimeout(asyncFn, 50);
+      }
+    };
+    return launchAsync ? asyncFn() : syncFn();
+  })
+
+};
+
+
+const N = (n || 10) * 10;
+const canvas = document.getElementById("canv");
+const context = canvas.getContext("2d");
+
+const horseImage = new Image();
+horseImage.src = './horse.png';
+
+canvas.width = N;
+canvas.height = N;
+
+const drawCanvas = () => {
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, N * 10, N * 10);
+  for (let i = 0; i < N; i += 10) {
+    for (let j = 0; j < N; j += 10) {
+      (((i + j) % 20) === 0 ) ?
+        context.fillStyle = "#462506" :
+        context.fillStyle = "#F2C661";
+      context.fillRect(i, j, 10, 10);
+    }
+  }
+};
+
+drawCanvas();
+
+const drawVisited = (point) => {
+  const x = point[0] * 10;
+  const y = point[1] * 10;
+  context.fillRect(x, y, 10, 10);
+};
+
+const drawHorse = (point) => {
+  const x = point[0] * 10;
+  const y = point[1] * 10;
+  context.fillStyle = "#ffffff";
+  context.globalAlpha = 1;
+  context.drawImage(horseImage, x, y, 10, 10);
+};
+
+const drawPath = (end, board, start) => {
+  const [xf, yf] = end;
+  let { prev } = board[xf][yf];
+  prev = prev || start;
+  const [xs, ys] = prev;
+
+  context.moveTo(10 * xs + 5, 10 * ys + 5);
+  context.lineTo(10 * xf + 5, 10 * ys + 5);
+  context.moveTo(10 * xf + 5, 10 * ys + 5);
+  context.lineTo(10 * xf + 5, 10 * yf + 5);
+
+  return ((xs === start[0]) && (ys === start[1])) ? null : drawPath(prev, board, start);
+};
+
+const m = new Map();
+
+const clickHandler = (e) => {
+
+  const { offsetX, offsetY } = e;
+
+  const key = m.size ? 2 : 1;
+
+  const xf = Math.floor(offsetX / 10);
+  const yf = Math.floor(offsetY / 10);
+
+  m.set(`${key}x`, xf);
+  m.set(`${key}y`, yf);
+
+  if (m.size === 2) {
+    drawCanvas();
+    drawHorse([xf, yf]);
+  } else if (m.size === 4) {
+    drawHorse([xf, yf]);
+    const xs = m.get('1x');
+    const ys = m.get('1y');
+
+    canvas.removeEventListener('click', clickHandler);
+
+    const timer = Date.now();
+    countSteps([xs, ys], [xf, yf], n, true)
+      .then((result) => {
+        const { value } = result[xf][yf];
+
+        document.getElementById('result').value = `${((Date.now() - timer) / 1000).toFixed(3)} s`;
+        document.getElementById('way').value = (value);
+
+        context.strokeStyle = "#fff";
+        context.beginPath();
+        drawPath([xf, yf], result, [xs, ys]);
+        context.stroke();
+        context.closePath();
+
+        drawHorse([xs, ys]);
+        drawHorse([xf, yf]);
+
+
+        context.stroke();
+        m.clear();
+        canvas.addEventListener('click', clickHandler);
+      });
+  }
+};
+
+
+canvas.addEventListener('click', clickHandler);
--- main.js
+++ main.js
@@ -1,38 +1,68 @@
-a_canvas.onclick = function(e) {
- m.push(e.offsetX==undefined?e.layerX:e.offsetX);
- m[m.length-1]=Math.floor((m[m.length-1])/10);
- m.push(e.offsetY==undefined?e.layerY:e.offsetY);
- m[m.length-1]=Math.floor(m[m.length-1]/10);
- if (m.length==2) { context.fillStyle = "#00ff00";
-                   context.fillRect(m[m.length-2]*10,m[m.length-1]*10,10,10);};
- if (m.length==4) { context.fillStyle = "#ff0000";
-                   context.fillRect(m[m.length-2]*10,m[m.length-1]*10,10,10);
-var timer = Date.now();
-var N = 1000;
-var xs = m[0];
-var ys = m[1];
-var xf = m[2];
-var yf = m[3];
-// Создаем двухмерный массив
-var chess = new Array(N);
-for (var i=N; i--;) {
-chess[i] = new Array(N);
+
+const N = 100;
+const a_canvas = document.getElementById("canv");
+const context = a_canvas.getContext("2d");
+
+a_canvas.width = N;
+a_canvas.height = N;
+
+for ( let i=0; i < N; i += 10) {
+  for (let j = 0; j < N; j += 10) {
+    (((i + j) % 20) ===0 ) ?
+    context.fillStyle = "#462506" :
+    context.fillStyle = "#F2C661";
+    context.fillRect(i, j, 10, 10);
+  };
 };
-var path = []; //Массив для хранения пути
-var count = 0,
-    steps;
-var pathsteps = [];
-function potentialMoves(xs, ys) {
-var trash = [];
- var   moves = [
-        [xs+2, ys+1],
-        [xs+2, ys-1],
-        [xs+1, ys+2],
-        [xs+1, ys-2],
-        [xs-1, ys+2],
-        [xs-1, ys-2],
-        [xs-2, ys+1],
-        [xs-2, ys-1]
+
+
+let m = [];
+
+const chess = new Array(N);
+
+for (let i = N; i--;) {
+  chess[i] = new Array(N);
+};
+
+
+a_canvas.onclick = (e) => {
+
+ m.push( e.offsetX == undefined ? e.layerX : e.offsetX);
+ m[m.length-1] = Math.floor((m[m.length-1]) / 10);
+
+ m.push( e.offsetY == undefined ? e.layerY : e.offsetY);
+ m[m.length-1]=Math.floor(m[m.length-1] / 10);
+
+ if (m.length === 2) {
+   context.fillStyle = "#00ff00";
+   context.fillRect(m[m.length-2] * 10, m[m.length-1] * 10, 10, 10);
+ } else if (m.length === 4) {
+   console.log(m)
+   context.fillStyle = "#ff0000";
+   context.fillRect(m[m.length-2] * 10, m[m.length-1] * 10, 10, 10);
+
+ var timer = Date.now();
+ let xs = m[0];
+ let ys = m[1];
+ let xf = m[2];
+ let yf = m[3];
+
+const path = [];
+let count = 0;
+let steps;
+const pathsteps = [];
+
+const potentialMoves = (xs, ys) => {
+  const trash = [];
+  const moves = [
+    [xs+2, ys+1],
+    [xs+2, ys-1],
+    [xs+1, ys+2],
+    [xs+1, ys-2],
+    [xs-1, ys+2],
+    [xs-1, ys-2],
+    [xs-2, ys+1],
+    [xs-2, ys-1]
     ];
             for (var j=moves.length; j--;) {
         if((moves[j][0]>=0) && (moves[j][0]<N) && (moves[j][1]>=0) && (moves[j][1]<N)) trash.push(moves[j]);
@@ -45,10 +75,16 @@ var trash = [];
        };
         };
 };
+
 potentialMoves(xs, ys);
+
+console.log({
+  chess, xf, yf, m
+})
+
 while (!chess[xf][yf]) {
-count+=1;
-var j=pathsteps.length;
+count += 1;
+var j = pathsteps.length;
 for (var i=j; i--;) {
     potentialMoves(pathsteps[i][0], pathsteps[i][1]);
 };
@@ -95,7 +131,7 @@ context.moveTo(10*xf+5,10*ys+5);
 context.lineTo(10*xf+5,10*yf+5);
 context.stroke();
 };
-context.stroke();             
+context.stroke();
 m=[];
                   };
-};
+                };
